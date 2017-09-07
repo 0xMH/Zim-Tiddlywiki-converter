7ac1d88279b396f18cc7ed495efd9c97194d8a1f
Hamza <12420351+0xMH@users.noreply.github.com>
2017-09-07 05:53:16 +0300

    fixed a typo

--- zim-tiddly-converter.sh
+++ zim-tiddly-converter.sh
@@ -5,7 +5,7 @@ convert ( ) {
         notxt=${nodir%.txt}
 
         creation_date=$(sed -n '3p' $1 | cut -d ' ' -f 2 | sed 's/[^[:digit:]]//g') #extract the creation date of Zim doc
-        title="$notxt" #$(sed -n '5p' $1| sed 's/ //g' | cut -d ' ' -f 2)                                   #extract the title of the Zim doc
+        title="$notxt" #$(sed -n '5p' $1| sed 's/ //g' | cut -d ' ' -f 2)           #extract the title of the Zim doc
 
         #making the new header by using change command on the originals
         #1st -e: put the creation date in first line
@@ -33,7 +33,7 @@ convert ( ) {
                                          # they also have special meaning in tiddly (make quotes).so I remove 'em too.
 
                                         # Repairing the Markup syntax.
-                                        gsed -E -e '6,$G'\
+                                        sed -E -e '6,$G'\
                                         -e "s/={6} (.*) ={6}/! \1/;"\
                                         -e "s/={5} (.*) ={5}/!! \1/"\
                                         -e "s/={4} (.*) ={4}/!!! \1/"\
